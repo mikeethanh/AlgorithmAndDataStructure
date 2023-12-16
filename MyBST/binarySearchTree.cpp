376c815b5ee6454a5efc240a8ddc2ac4674cb018
mikee thanh <mikeethanh04@gmail.com>
2023-12-16 23:15:47 +0700

    addBST

--- MyBST/binarySearchTree.cpp
+++ MyBST/binarySearchTree.cpp
@@ -84,7 +84,7 @@ class BinarySearchTree {
     // tra ve min key trong tree
     BSTNode<Key,Data> *minKey() ;
     //tra ve max key trong tree
-    BSTNode<Key.Data> *maxKey();
+    BSTNode<Key,Data> *maxKey();
 
     public: 
     //Constructor:
@@ -99,7 +99,7 @@ class BinarySearchTree {
     int isExternal(BSTNode<Key,Data> *p);
     //search dua tren key 
     // truyen node root vao ham de co the so sanh key > ket root hay < key root de sang keft or right 
-    BSTNode<Key,Data>* search(key, BSTNode<Key,Data>* p);
+    BSTNode<Key,Data>* search(Key key);
     //truyen key va data :  
     void insert(Key key , Data data);
     //check xem 1 node co la root hay khong 
@@ -190,10 +190,10 @@ BSTNode<Key,Data>* BinarySearchTree<Key,Data> ::minKey() {
 }
 
 template<class Key,class Data> 
-BSTNode<Key,Data>* BinarySearchTree<Key,Data> ::minKey() {
+BSTNode<Key,Data>* BinarySearchTree<Key,Data> ::maxKey() {
     BSTNode<Key,Data> *temp 
     temp = root
-    while(temp->getRight() != nullptr) {
+    while(temp->getRight()  != nullptr) {
         temp = temp->getRight();
     }
     return temp ; 
@@ -246,4 +246,124 @@ void BinarySearchTree<Key,Data> ::insert(Key key , Data data) {
             }
         }
     }
+}
+
+//xay dung function search:
+template <class Key, class Data> 
+BSTNode<Key,Data>* BinarySearchTree<Key,Data> ::search(Key key) {
+    //khai bao mot con tro bsnode tro toi dia chi node root 
+    BSTNode<Key,Data>* temp;
+    while(temp != nullptr) {
+        if(key < temp->getKey()) {
+            temp = temp -> getLeft();
+        }
+        else if(key > temp->getKey()) {
+            temp = temp->getRight();
+        }else {
+            return temp ; 
+        }
+    }
+    //khong tim thay 
+    return nullptr;
+}
+
+//xoa node theo node duong ap dung e xoa node theo ki , vi the se xay dung ham xoa node them node truoc  : 
+//th1 : node khong co trong tree
+//th2 : node la node la => xet con tro tro den parent cua node . neu parent == null => xoa node root , neu parent != null thi xet xem node do nam ben left or right,
+//sau o xoa node
+//th3: key la node co 1 child => cho 1 con tro tro den node can xoa va 1 con tro tro den node child cua no sau do xoa node can xoa va noi not child cua node can xoa 
+
+
+
+template<class Key,class Data> 
+void BinarySearchTree<Key,Data> ::remove(BSTNode<Key,Data> *&p) {
+    BSTNode<Key,Data> *temp;
+    //TH1 : Node leaf:
+    if(!p->hasLeft && !p->hasRight) {
+        temp = v->getParent();
+        if(temp != nullptr) {
+            if(temp->getLeft() == p) {
+                temp->setLeft(nullptr);
+            }else {
+                temp->setRight(nullptr);
+            }
+        }else{
+            root = nullptr;
+        }
+    }
+    //TH2: Node have one left child : 
+    if(p->hasLeft && !p->hasRight) {
+        temp = p->getParent();
+        p->getLeft()->setParent(temp);
+        if(temp->getLeft() == p) {
+            temp->setLeft(p->getLeft());
+        }else {
+            temp->setRight(p->getLeft());
+        }
+    }
+    //TH3: Node have one right child : 
+    if(!p->hasLeft() && p->hasRight()) {
+        temp = p->getParent();
+        p->getRight()->setParent(temp);
+        if(temp->getLeft() == p) {
+            temp->setLeft(p->getRight());
+        }else {
+            temp->setRight(p->getRight());
+        }
+    }
+}
+
+//xoa node theo key : 
+//tim xem key do co trong key khong ; neu co moi thuc hien ham remove ben tren , neu khong thi return ve luon 
+// th4 : neu node can xoa co 2 node child ; tim node con nho nhat ben phai node can xoa sau do lau key va value cuar node do thay cho node can xoa 
+//xoa node do di ; 
+
+template<class Key,class Data> 
+void BinarySearchTree<Key,Data> ::remove(Key key) {
+    BSTNode<Key,Data>* p = search(key);
+    if(p->hasLeft && p->hasRight) {
+        BSTNode<Key,Data>* temp ;
+        temp = minKey(p->getRight());
+        p->setKey(temp->getKey);
+        p->setData(temp->getData);
+        remove(temp);
+    }else {
+        remove(v);
+    }
+    cout--;
+}
+
+//xay dung ham duyet theo thu tu inorder(left-> print->right)
+template<class Key,class Data>
+void inOrderTraversal(BSTNode<Key,Data> *p ,void (*visit) (BSTNode<Key,Data> *)) {
+    if(p != nullptr) {
+        inOrderTraversal(p->getLeft,visit);
+        visit(p);
+        inOrderTraversal(p->getRight,visit);
+    }    
+}
+
+//preorder(print->left-right)
+template<class Key,class Data>
+void preOrderTraversal(BSTNode<Key,Data> *p ,void (*visit) (BSTNode<Key,Data> *)) {
+    if(p != nullptr) {
+         visit(p);
+        inOrderTraversal(p->getLeft,visit);
+        inOrderTraversal(p->getRight,visit);
+    }    
+}
+
+//postOrder(left->right->print) 
+template<class Key,class Data>
+void preOrderTraversal(BSTNode<Key,Data> *p ,void (*visit) (BSTNode<Key,Data> *)) {
+    if(p != nullptr) {
+        inOrderTraversal(p->getLeft,visit);
+        inOrderTraversal(p->getRight,visit);
+        visit(p);
+    }    
+}
+
+
+void v(BSTNode<int,char*> *p) {
+    cout << p->getKey() << "," << p->getData();
 }
