f0de755da9e1e251aeb612206b4f3f9abd652335
mikeethanh <109719604+mikeethanh@users.noreply.github.com>
2023-12-21 19:09:19 +0700

    addFodler

--- MyVector/myVector.cpp
+++ MyVector/myVector.cpp
@@ -1,130 +1,151 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
+//xay dun lop bo duyet nguoc 
+template<class T> 
+class VRI {
+    T* curr; // bo duyet tro den node hien tai 
+    public:
+    //ham khoi tao 1 tham so 
+    vector_reverse_iterator(T *node = nullptr) : curr(node) {};
 
-template <class T>
-class vector_reverse_iterator
-{
-    T *curr;
-public:
-    vector_reverse_iterator(T *c = nullptr) : curr(c) {}
-    
-    vector_reverse_iterator<T> &operator=(vector_reverse_iterator<T> it)
-    {
+    //nap toan tu = 
+    VRI<T> &operator = (VRI<T> it) {
         this->curr = it.curr;
         return *this;
     }
-    
-    vector_reverse_iterator<T> operator++() // ++curr
-    {
+
+    //nap chong toan tu ++ hau to  :
+    VRI<T> &operator++() {
         curr--;
-        return *this;
+        return curr;
     }
-    
-    vector_reverse_iterator<T> operator++(int) // ++curr
-    {
-        vector_reverse_iterator<T> tmp = *this;
+
+    // nap chon toan tu hau to : 
+    VRI<T> &operator++(int) {
+        VRI<T> tmp = curr;
         curr--;
         return tmp;
     }
-    
-    T &operator*() { return *curr; }
-    
+
+    // nap chon toan tu * tra ve phan tu ben trong con tro curr dan tro toi 
+    T &operator *() {
+        return *curr;
+    }
+
     bool operator!=(vector_reverse_iterator<T> t) { return curr != t.curr; }
 };
 
-template<class T>
-class Vector
-{
-private:
-    int cap, num;
-    T *buff;
-    
-public:
-    Vector() : cap(0), num(0), buff(nullptr) {}
-    
-    Vector(int k, T x) : cap(k), num(k), buff(new T[k])
-    {
-        for (int i = 0; i < k; i++) buff[i] = x;
+
+//Xây dưng class Vector dưới dạng mảng động 
+template <class T>
+class Vector {
+    private:
+    int cap ;  //Suc chua 
+    int num ; //so luong phan tu 
+    T *buff; //con tro buff , su dung de quan li cac phan tu trong mang dong 
+    public:
+    //constructor : 
+    Vector() : cap(0) , num(0) , buff(nullptr) {};
+    //constructor have 2 para ;
+    Vector(int cap , int num ) : cap(cap) , num(num) buff(new T[cap]) {
+        for (int i = 0; i < cap; i++)  buff[i] = num
+    } 
+    //constructor have 1 para 
+    Vector()
+    //destructor : 
+    ~Vector() {
+        if(buff) delete[] buff;
     }
+
+    // phuong thuc : 
+
+    // tra ve suc chua 
+    int capacity() { return cap};
     
-    ~Vector() { if (buff) delete[] buff; }
-    
-    int capacity() { return cap; }
-    
-    int size() { return num; }
-    
-    bool empty() { return num == 0; }
-    
-    void pop_back() { if (num > 0) num--; }
-    
-    void extend(int newcap)
-    {
-        if (newcap < cap) return;
+    //tra ve size 
+    int size() { return num; };
+    //check Empty
+    bool isEmpty() { return num == 0; };
+    //pop_back : pop phan tu cuoi cun ra khoi vector 
+    void pop_back() {
+        if(num > 0) {
+            num--;
+            return;
+        }
+    }
+    //extend : neu nhu muon them 1 phan tu vao ma suc chua khong du se ggoi ham extend de mo rong duc chua 
+    void extend(int newcap) {
+        if(newcap =< cap) {
+            return ; 
+        }
+        // neu newcap > cap 
         cap = newcap;
-        T *temp = new T[cap];
-        for (int i = 0; i < num; i++) temp[i] = buff[i];
-        if (buff) delete[] buff;
+        //khai bao 1 doi mang dong moi voi tham so la cap
+
+        //copy cac phan tu sang mang don moi 
+        T* temp = new T[cap];
+        for(int i = 0 ; i < num ; i++) {
+            temp[i] = buff[i];
+        } 
+        // xoa buff 
+        if(buff) {
+            delete[] buff;
+        }
+        //
         buff = temp;
     }
-    
-    T &back() { return buff[num - 1]; }
-    
-    void push_back(T x)
-    {
-        if (num == cap) extend(cap * 2 + 5);
-        buff[num++] = x;
+
+    // lay ra phan tu cuoi con 
+
+    T *back() {
+        return buff[num-1];
     }
-    
-    T &operator[](int k) { return buff[k]; }
-    
-    void insert(int k, T x)
-    {
-        if (cap == num) extend(cap * 2 + 5);
-        for (int i = num - 1; i >= k; i--) buff[i + 1] = buff[i];
-        buff[k] = x;
-        num++;
-    }
-    
-    Vector &operator=(Vector<T> V)
-    {
-        this->num = V.num;
-        this->cap = V.cap;
-        if (cap)
-        {
-            this->buff = new T[cap];
-            for (int i = 0; i < num; i++) this->buff[i] = V.buff[i];
+
+    //void day 1 phan tu vao cuoi 
+    void push_back(T newElem) {
+        if(cap == num) {
+            extend(cap*2 + 5);
+        } else {
+            buff[num] = newElem;
+            num++;
         }
-        else this->buff = nullptr;
-        return *this;
     }
+
+    //cach xay dun operator[]
+    // ten lop operator,,() {}
     
-    T *begin() { return buff; }
-    
-    T *end() { return buff + num; }
-    
-    vector_reverse_iterator<T> rbegin() { return vector_reverse_iterator<T>(buff + num - 1); }
-    
-    vector_reverse_iterator<T> rend() { return vector_reverse_iterator<T>(buff - 1); }
+    T &operator[](int k) {
+        return buff[k];
+    }
+
+    //insert vao vi tri n 1 so 
+    void insert(int index , T elem) {
+        if(cap == num) {
+            extend(cap*2 + 5);
+        }else {
+            for(int i = num - 1 ; i >= index; i--) {
+                buff[i+1] = buff[i];
+            }
+            buff[k] = x ; 
+        }
+    }
+
+    //nap chong toan tu de copy vector hien tai ra 1 vector khac :
+    Vector &operator=(Vector<T> v) {
+        this->num = v.num;
+        this->cap = v.cap;
+        if(cap) {
+            // khai bao 1 man dong moi voi dun luong ban cap
+            this->buff = new T[cap];
+            //an cac phan tu o buff cho cap 
+            for (int i = 0 ; i < num , i++) {
+                buff[i] = V.buff[i];
+            }           
+        }
+    }
 };
 
-int main()
-{
-    Vector<int> V(7, 6);
-    cout << "\nVector ban dau:";
-    for (int i = 0; i < V.size(); i++) cout << V[i] << " ";
-    for (int i = 0; i < V.size(); i++) V[i] = i * i;
-    cout << "\nVector binhphuong:";
-    for (int i = 0; i < V.size(); i++) cout << V[i] << " ";
-    V.push_back(11);
-    V.push_back(13);
-    V.insert(2, 20);
-    cout << "\nVector :";
-    for (int i = 0; i < V.size(); i++) cout << V[i] << " ";
-    cout << "\nDuyet :";
-    for (int *it = V.begin(); it != V.end(); it++) cout << *it << "\t";
-    cout << "\nDuyet auto : ";
-    for (auto x : V) cout << x << " ";
-    cout << "\nNguoc : ";
-    for (auto it = V.rbegin(); it != V.rend(); it++) cout << *it << " ";
-    return 0;
+
+int main() {
+
 }
--- MyVector/myVector2.cpp
+++ MyVector/myVector2.cpp
@@ -1,101 +1,130 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-//Xây dưng class Vector dưới dạng mảng động 
 template <class T>
-class Vector {
-    private:
-    int cap ;  //Suc chua 
-    int num ; //so luong phan tu 
-    T *buff; //con tro buff , su dung de quan li cac phan tu trong mang dong 
-    public:
-    //constructor : 
-    Vector() : cap(0) , num(0) , buff(nullptr) {};
-    //constructor have 2 para ;
-    Vector(int cap , int num ) : cap(cap) , num(num) buff(new T[num]) {
-        for (int i = 0; i < cap; i++)  buff[i] = num
-    } 
-    //constructor have 1 para 
-    Vector()
-    //destructor : 
-    ~Vector() {
-        if(buff) delete[] buff;
-    }
-
-    // phuong thuc : 
-
-    // tra ve suc chua 
-    int capacity() { return cap};
+class vector_reverse_iterator
+{
+    T *curr;
+public:
+    vector_reverse_iterator(T *c = nullptr) : curr(c) {}
     
-    //tra ve size 
-    int size() { return num; };
-    //check Empty
-    bool isEmpty() { return num == 0; };
-    //pop_back : pop phan tu cuoi cun ra khoi vector 
-    void pop_back() {
-        if(num > 0) {
-            num--;
-            return;
-        }
+    vector_reverse_iterator<T> &operator=(vector_reverse_iterator<T> it)
+    {
+        this->curr = it.curr;
+        return *this;
     }
-    //extend : neu nhu muon them 1 phan tu vao ma suc chua khong du se ggoi ham extend de mo rong duc chua 
-    void extend(int newcap) {
-        if(newcap =< cap) {
-            return ; 
-        }
-        // neu newcap > cap 
-        cap = newcap;
-        //khai bao 1 doi mang dong moi voi tham so la cap
-
-        //copy cac phan tu sang mang don moi 
-        T* temp = new T[cap];
-        for(int i = 0 ; i < num ; i++) {
-            temp[i] = buff[i];
-        } 
-        // xoa buff 
-        if(buff) {
-            delete[] buff;
-        }
-        //
-        buff = temp;
-    }
-
-    // lay ra phan tu cuoi con 
-    T *back() {
-        return buff[num-1];
-    }
-
-    //void day 1 phan tu vao cuoi 
-    void push_pack(T newElem) {
-        if(cap == num) {
-            extend(cap*2 + 5);
-        } else {
-            buff[num] = newElem;
-            num++;
-        }
-    }
-
-    //cach xay dun operator[]
-    // ten lop operator,,() {}
     
-    T &operator[](int k) {
-        return buff[k];
+    vector_reverse_iterator<T> operator++() // ++curr
+    {
+        curr--;
+        return *this;
     }
-
-    //insert vao vi tri n 1 so 
-    void insert(int index , T elem) {
-        if(cap == num) {
-            extend(cap*2 + 5);
-        }else {
-            for(int i = num - 1 ; i >= k; i--) {
-                buff[i+1] = buff[i];
-            }
-            buff[k] = x ; 
-        }
+    
+    vector_reverse_iterator<T> operator++(int) // ++curr
+    {
+        vector_reverse_iterator<T> tmp = *curr;
+        curr--;
+        return tmp;
     }
+    
+    T &operator*() { return *curr; }
+    
+    bool operator!= (vector_reverse_iterator<T> t) { return curr != t.curr; }
 };
 
+template<class T>
+class Vector
+{
+private:
+    int cap, num;
+    T *buff;
+    
+public:
+    Vector() : cap(0), num(0), buff(nullptr) {}
+    
+    Vector(int k, T x) : cap(k), num(k), buff(new T[k])
+    {
+        for (int i = 0; i < k; i++) buff[i] = x;
+    }
+    
+    ~Vector() { if (buff) delete[] buff; }
+    
+    int capacity() { return cap; }
+    
+    int size() { return num; }
+    
+    bool empty() { return num == 0; }
+    
+    void pop_back() { if (num > 0) num--; }
+    
+    void extend(int newcap)
+    {
+        if (newcap < cap) return;
+        cap = newcap;
+        T *temp = new T[cap];
+        for (int i = 0; i < num; i++) temp[i] = buff[i];
+        if (buff) delete[] buff;
+        buff = temp;
+    }
+    
+    T &back() { return buff[num - 1]; }
+    
+    void push_back(T x)
+    {
+        if (num == cap) extend(cap * 2 + 5);
+        buff[num++] = x;
+    }
+    
+    T &operator[](int k) { return buff[k]; }
+    
+    void insert(int k, T x)
+    {
+        if (cap == num) extend(cap * 2 + 5);
+        for (int i = num - 1; i >= k; i--) buff[i + 1] = buff[i];
+        buff[k] = x;
+        num++;
+    }
+    
+    Vector &operator=(Vector<T> V)
+    {
+        this->num = V.num;
+        this->cap = V.cap;
+        if (cap)
+        {
+            this->buff = new T[cap];
+            for (int i = 0; i < num; i++) this->buff[i] = V.buff[i];
+        }
+        else this->buff = nullptr;
+        return *this;
+    }
+    
+    T *begin() { return buff; }
+    
+    T *end() { return buff + num; }
+    
+    vector_reverse_iterator<T> rbegin() { return vector_reverse_iterator<T>(buff + num - 1); }
+    
+    vector_reverse_iterator<T> rend() { return vector_reverse_iterator<T>(buff - 1); }
+};
 
-int main() {
-
+int main()
+{
+    Vector<int> V(7, 6); 
+    cout << "\nVector ban dau:";
+    for (int i = 0; i < V.size(); i++) cout << V[i] << " ";
+    for (int i = 0; i < V.size(); i++) V[i] = i * i;
+    cout << "\nVector binhphuong:";
+    for (int i = 0; i < V.size(); i++) cout << V[i] << " ";
+    V.push_back(11);
+    V.push_back(13);
+    V.insert(2, 20);
+    cout << "\nVector :";
+    for (int i = 0; i < V.size(); i++) cout << V[i] << " ";
+    cout << "\nDuyet :";
+    for (int *it = V.begin(); it != V.end(); it++) cout << *it << "\t";
+    cout << "\nDuyet auto : ";
+    for (auto x : V) cout << x << " ";
+    cout << "\nNguoc : ";
+    for (auto it = V.rbegin(); it != V.rend(); it++) cout << *it << " ";
+    return 0;
 }
